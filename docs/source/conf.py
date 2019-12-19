21c0684185bfd3ff7b853daf8cf796f403d8db23
Wind Waver <waverwind@gmail.com>
2019-12-19 19:58:56 +0100

    asdf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,6 +54,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+master_doc = 'index'
 
 locale_dirs = ['locale/']   # path is example but recommended.
 gettext_compact = False     # optional.
